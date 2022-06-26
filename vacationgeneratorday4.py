323ba4d70646131c9b6d5a2c624341e00038a697
Devin Chen <taunwe79@gmail.com>
2022-06-26 13:47:28 -0500

    "Added project parameters and lists"

---
+++ vacationgeneratorday4.py
@@ -0,0 +1,49 @@
+# Developer Objectives (5 points each objective)
+
+# As a developer, I want to make at least three commits with descriptive messages. 
+# As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainments in their own separate lists. (DONE)
+# As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
+
+# User Objectives (5 points each objective)
+
+# As a user, I want a destination to be randomly checked for my day trip. 
+# As a user, I want a restaurant to be randomly selected for my day trip. 
+# As a user, I want a mode of transportation to be randomly selected for my day trip. 
+# As a user, I want a form of entertainment to be randomly selected for my day trip. 
+
+# User Objectives (10 points each objective) - Preferably write the two separate 
+
+# As a user, I want to be able to confirm that my day trip is "complete" once I like all of the random selections. 
+# As a user, I want to display my completed trip in the console. 
+
+# User Objectives (15 points each objective)
+
+# As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don't like one or more of those things. 
+
+
+
+
+
+# List for possible vacation destinations
+vacation_destinations = ["Aruba", "Bermuda", "Chile", "Denmark"]
+
+# Lists of Aruban options
+Aruba_restaurants = ["Water's Edge Restaurant & Bar Aruba", "Fred Royal Aruba", "L.G. Smith's Steak & Chop House", "Ike's Bistro"]
+Aruba_mode_of_transporation = ["plane", "boat", "submarine"]
+Aruba_entertainment = ["ATV Off Road Exploring and Snorkeling", "Aruba Aloe Factory", "Renaissance Island", "The Butterfly Farm"]
+
+# Lists of Bermudan options
+Bermuda_restaurants = ["La Trattoria", "Beluga Sushi Bar", "1609 Bar and Restaurant", "Flanagan's Irish Pub"]
+Bermuda_mode_of_transportation = ["plane", "boat"]
+Bermuda_entertainment = ["Bermuda Acquarium and Crystal Caves", "Horshoe Bay Beach", "Hartley Helmet Diving", "Shipwreck Snorkeling"]
+
+# Lists of Chilean options 
+Chile_restaurants = ["Restaurante 040", "Borago", "99 Restaurante", "Ambrosia"]
+Chile_mode_of_transportation = ["plane", "boat", "car"]
+Chile_entertainment = ["Torres Del Paine In Patagonia", "Isla Magdalena", "Street Art In Valparaiso", "Cajon Del Maipo"]
+
+# Lists of Danish options
+Denmark_restaurants = ["Basso Kobenhavn", "Restaurant Krebsegaarden", "Den Lille Fede Restaurant", "Restaurant Maven"]
+Denmark_mode_of_transportation = ["plane", "train", "car", "boat", "bus", "cycling"]
+Denmark_entertainment = ["Copenhagen Zoo", "Denmark National Park", "Rabjerg Mile", "Egeskov Castle in Funen"]
+
